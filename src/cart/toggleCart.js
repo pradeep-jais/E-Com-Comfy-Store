1c125c8efa2201dd78bee560ce8b52406bae909f
pradeep-jais <pradeepjais13@gmail.com>
2023-04-04 23:05:59 +0530

    about page setup complete, featured section html complete

--- src/cart/toggleCart.js
+++ src/cart/toggleCart.js
@@ -1,3 +1,14 @@
 import { getElement } from '../utils.js';
 
+const cartOverlay = getElement('.cart-overlay');
+const closeCartBtn = getElement('.cart-close');
+const toggleCartBtn = getElement('.toggle-cart');
+
+toggleCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.add('show');
+});
+closeCartBtn.addEventListener('click', () => {
+  cartOverlay.classList.remove('show');
+});
+
 export const openCart = () => {};
